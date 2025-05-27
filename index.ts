a38e4864d6641ace0d923188c0cf5cb572ac1801
anmolshres98 <anmolshres98@users.noreply.github.com>
2025-05-27 11:32:00 -0500

    get after insertion error

--- index.ts
+++ index.ts
@@ -1,5 +1,7 @@
-import { IModelDb, IModelHost, IModelHostOptions, IModelJsFs, SpatialCategory, StandaloneDb } from "@itwin/core-backend";
+import { CategorySelector, DefinitionModel, DisplayStyle2d, DrawingCategory, DrawingViewDefinition, IModelHost, IModelHostOptions, IModelJsFs, StandaloneDb, Subject } from "@itwin/core-backend";
 import { Logger, LogLevel } from "@itwin/core-bentley";
+import { IModel, SubCategoryAppearance } from "@itwin/core-common";
+import { Range2d } from "@itwin/core-geometry";
 
 async function main() {
   console.log("Starting iTwin Core Backend Demo");
@@ -17,20 +19,47 @@ async function main() {
     const iModel = StandaloneDb.createEmpty(sourcePath, {
       rootSubject: { name: sourceFileName },
     });
-
-    // Not throwing an error
-    iModel.performCheckpoint();
-
-    SpatialCategory.insert(
+    const subjectId = Subject.insert(
       iModel,
-      IModelDb.dictionaryId,
-      "spatial category",
-      {}
+      IModel.rootSubjectId,
+      "Subject",
+      "Subject Description"
+    );
+    const definitionModelId = DefinitionModel.insert(
+      iModel,
+      subjectId,
+      "Definition"
+    );
+    const drawingCategoryId = DrawingCategory.insert(
+      iModel,
+      definitionModelId,
+      "DrawingCategory",
+      new SubCategoryAppearance()
+    );
+    const drawingCategorySelectorId = CategorySelector.insert(
+      iModel,
+      definitionModelId,
+      "DrawingCategories",
+      [drawingCategoryId]
+    );
+    const displayStyle2dId = DisplayStyle2d.insert(
+      iModel,
+      definitionModelId,
+      "DisplayStyle2d"
     );
 
-    iModel.saveChanges("Created spatial category");
-    // Throwing error: BE_SQLITE_LOCKED in rc; doesn't throw in latest official release
-    iModel.performCheckpoint();
+    const drawingViewId = DrawingViewDefinition.insert(
+      iModel,
+      definitionModelId,
+      "Drawing View",
+      "0",
+      drawingCategorySelectorId,
+      displayStyle2dId,
+      new Range2d(0, 0, 100, 100)
+    );
+    console.log("Drawing View Definition created with ID:", drawingViewId);
+    iModel.elements.getElement(drawingViewId);
+
     iModel.close();
 
     await IModelHost.shutdown();
